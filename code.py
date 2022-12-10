e6cec961b65c76c8fdb89ff696dc921ea8ab721c
Jannatbijral <119727955+Jannatbijral@users.noreply.github.com>
2022-12-10 21:06:42 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,14 @@
-# fibonacci_series
-#it is a series of sum of previous two consecutive no.
-#program to print fibonacci series
 
-#defining a function
 def fib(n):
     a = 0
     b = 1
-#using conditional statements
+
     if n == 1:
         print(a)
     else:
         print(a)
         print(b)
-#using looping statements
+
         for i in range(2,n):
             c = a + b
             a = b
